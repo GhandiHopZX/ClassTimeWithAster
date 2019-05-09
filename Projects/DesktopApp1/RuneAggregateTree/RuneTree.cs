39eb847d713f61a5c7ae170f4558a2959a901acd
Solomon Pattee <solomon.pattee@my.tccd.edu>
2019-05-09 02:43:22 -0500

    ok

--- Projects/DesktopApp1/RuneAggregateTree/RuneTree.cs
+++ Projects/DesktopApp1/RuneAggregateTree/RuneTree.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace RuneAggregateTree
 {
-    public class Class1
+    public class RuneTree
     {
         struct Rune
         {
             string Type; // dictates the type of Rune it is 
-            int ID; // dictates the name and the number
+            private int ID; // dictates the name and the number
         }
 
         struct Spell
