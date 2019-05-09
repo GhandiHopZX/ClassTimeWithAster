a243042e434d0a7cee4c10326bc04a663a40132e
solomon pattee <solomon.pattee@my.tccd.edu>
2019-05-08 19:48:02 -0500

    OK lol

---
+++ Projects/DesktopApp1/RuneAggregateTree/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuneAggregateTree
+{
+    public class Class1
+    {
+        struct Rune
+        {
+            string Type; // dictates the type of Rune it is 
+            int ID; // dictates the name and the number
+        }
+
+        struct Spell
+        {
+            string RType;
+            string RClass;
+            int SID;
+            string Essence;
+        }
+    }
+}
